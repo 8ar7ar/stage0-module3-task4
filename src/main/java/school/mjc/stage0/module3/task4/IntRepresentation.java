d4285d85015e1fb1a378d4f124007ddefb1d9f70
8ar7ar <5ar7ar@mail.ru>
2023-10-19 21:20:12 +0500

    stage0.module3.task4 "2nd attempt"

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -5,9 +5,11 @@ public class IntRepresentation {
         char c = 99;
         char h = 104;
         char a = 97;
+        char r = 114;
         System.out.println(c);
         System.out.println(h);
         System.out.println(a);
+        System.out.println(r);
 
     }
 }
